3e3350bf638836d8cf9262c4f82dac808604dea9
kumm,Rama <rama.komm@gmail.com>
2018-11-29 17:42:46 -0800

    Simplr

---
+++ tryMe/trail/trailing.py
@@ -0,0 +1,9 @@
+print("Hellooooooo")
+for i in range(10):
+    print("we are doing very well   ",i)
+def fact(num):
+    prod = 1
+    for i in range(1,num):
+        prod = prod * i
+    print(prod)
+fact(20)
